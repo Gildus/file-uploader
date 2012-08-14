b676f3096fed7369c0e6632780d04a16a4095cb9
Gildus <gildus@gmail.com>
2012-08-13 21:51:51 -0500

    Fix to ['CONTENT_LENGTH']

--- server/php.php
+++ server/php.php
@@ -50,13 +50,16 @@ class qqUploadedFileXhr {
     function getName() {
         return $_GET['qqfile'];
     }
-    function getSize() {
-        if (isset($_SERVER["CONTENT_LENGTH"])){
-            return (int)$_SERVER["CONTENT_LENGTH"];            
+    function getSize() {                
+        if (isset($_SERVER["CONTENT_LENGTH"]) || isset($_SERVER['HTTP_CONTENT_LENGTH'])){
+            if(isset($_SERVER['HTTP_CONTENT_LENGTH']))
+                return (int)$_SERVER["HTTP_CONTENT_LENGTH"];
+            else
+                return (int)$_SERVER["CONTENT_LENGTH"];
         } else {
             throw new Exception('Getting content length is not supported.');
-        }      
-    }   
+        }
+    }  
 }
 
 /**
